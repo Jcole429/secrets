ee995ab96f1fe66f068f294ea52b9c5dc86125bd
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-28 22:46:26 -0400

    Implement level 3 authentication Hashing passwords with md5

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require("path");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -21,8 +21,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,7 +41,7 @@ app.route("/login")
             if (foundUser === null) {
                 res.send("User not found");
             } else {
-                if (foundUser.password === req.body.password) {
+                if (foundUser.password === md5(req.body.password)) {
                     res.render("secrets");
                 } else {
                     res.send("Incorrect password");
@@ -59,7 +57,7 @@ app.route("/register")
     .post((req, res) => {
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
 
         newUser.save().then((newUser) => {
